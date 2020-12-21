60c5c9b87276dc8a85354e316786602f45913f1b
Sinkem <61325685+Sinkem@users.noreply.github.com>
2020-12-21 18:45:53 +0100

    Update ScapeRoom.java

--- ScapeRoom/src/scaperoom/ScapeRoom.java
+++ ScapeRoom/src/scaperoom/ScapeRoom.java
@@ -1,3 +1,11 @@
+/*Aqui iniciamos la Scape Room
+ *Para salir del juego se hace mediante la x en la esquina de la ventana.
+ *
+ *Me habría molado meter muchas mas cosas, pero me veia muy justo de tiempo.
+ *
+ *El camino mas rapido es ir izquierda, derecha, izquierda, pagar, delante.
+ */
+
 package scaperoom;
 
 public class ScapeRoom {
